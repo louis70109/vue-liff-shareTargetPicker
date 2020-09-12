396d7e5e5f8ae465014271f5f5a997894c25bdd4
NiJia <louis70109@gmail.com>
2020-09-12 12:57:44 +0800

    feat: add share target picker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,62 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="sendTargetPicker">Send Sample</button>
   </div>
 </template>
 
 <script>
+import { onMounted } from "vue";
+import liff from "@line/liff";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  setup() {
+    onMounted(async () => {
+      try {
+        await liff.init({ liffId: "YOUR_LIFF_ID" });
+        if (!liff.isLoggedIn())
+          liff.login({ redirectUri: window.location.href });
+      } catch (err) {
+        console.log(`liff.state init error ${err}`);
+      }
+    });
+    async function sendTargetPicker() {
+      if (!liff.isLoggedIn()) {
+        liff.login({ redirectUri: window.location.href });
+      }
+      if (liff.isApiAvailable("shareTargetPicker")) {
+        try {
+          const picker = await liff.shareTargetPicker([
+            {
+              type: "text",
+              text: "Hello, World!",
+            },
+          ]);
+          if (picker) {
+            // succeeded in sending a message through TargetPicker
+            console.log(`[${picker.status}] Message sent!`);
+          } else {
+            const [majorVer, minorVer] = (liff.getLineVersion() || "").split(
+              "."
+            );
+            if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
+              console.log(
+                "TargetPicker was opened at least. Whether succeeded to send message is unclear"
+              );
+            } else console.log("TargetPicker was closed!");
+          }
+        } catch (error) {
+          // something went wrong before sending a message
+          console.log(error);
+          console.log("Flex Message got some error");
+          liff.closeWindow();
+        }
+      } else console.log("Please login...");
+    }
+    return {
+      sendTargetPicker,
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
