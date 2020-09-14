12dee4f95bfeaaf5a916d307ed5305393e9b2d77
NiJia Lin <louis70109@gmail.com>
2020-09-14 20:52:25 +0800

    feat: use env variable

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="hello">
-    <button @click="sendTargetPicker">Send Sample</button>
-  </div>
+<div class="hello">
+  <button @click="sendTargetPicker">Send Sample</button>
+</div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import {
+  onMounted
+} from "vue";
 import liff from "@line/liff";
 export default {
   name: "HelloWorld",
   setup() {
     onMounted(async () => {
       try {
-        await liff.init({ liffId: "YOUR_LIFF_ID" });
+        await liff.init({
+          liffId: process.env.SHARE_ID
+        });
         if (!liff.isLoggedIn())
-          liff.login({ redirectUri: window.location.href });
+          liff.login({
+            redirectUri: window.location.href
+          });
       } catch (err) {
         console.log(`liff.state init error ${err}`);
       }
     });
     async function sendTargetPicker() {
       if (!liff.isLoggedIn()) {
-        liff.login({ redirectUri: window.location.href });
+        liff.login({
+          redirectUri: window.location.href
+        });
       }
       if (liff.isApiAvailable("shareTargetPicker")) {
         try {
-          const picker = await liff.shareTargetPicker([
-            {
-              type: "text",
-              text: "Hello, World!",
-            },
-          ]);
+          const picker = await liff.shareTargetPicker([{
+            type: "text",
+            text: "Hello, World!",
+          }, ]);
           if (picker) {
             // succeeded in sending a message through TargetPicker
             console.log(`[${picker.status}] Message sent!`);
@@ -60,18 +66,22 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
